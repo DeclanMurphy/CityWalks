278b8cf60adb751c00f854ae2ee57f5be7f839d6
Declan <Declan@declan.declan>
2014-02-05 19:40:53 +0000

    POJOs

---
+++ CityWalks/src/com/example/citywalks/Coordinate.java
@@ -0,0 +1,35 @@
+package com.example.citywalks;
+
+public class Coordinate {
+	
+	private double latitude;
+	private double longitude;
+	private int coordinateId;
+	
+	
+	public Coordinate(double latitude, double longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public int getCoordinateId() {
+		return coordinateId;
+	}
+	public void setCoordinateId(int coordinateId) {
+		this.coordinateId = coordinateId;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+}
